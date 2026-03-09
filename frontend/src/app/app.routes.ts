ccc0c5712d4d8cdbaf8a1da6730f577be477f088
Broderick Nana Bentil <bentilfrederick4@gmail.com>
2026-03-09 18:00:39 +0000

    refactor: update app component and routes configuration

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,19 @@
 import { Routes } from "@angular/router";
-import { PollListComponent } from "./pages/poll-list.component";
-import { LoginComponent } from "./pages/login.component";
-import { RegisterComponent } from "./pages/register.component";
-import { CreatePollComponent } from "./pages/create-poll.component";
+import { LoginComponent } from "./auth/pages/login/login.component";
+import { RegisterComponent } from "./auth/pages/register/register.component";
 
 export const routes: Routes = [
-  { path: "", component: PollListComponent },
-  { path: "login", component: LoginComponent },
-  { path: "register", component: RegisterComponent },
-  { path: "create", component: CreatePollComponent },
+  {
+    path: "",
+    redirectTo: "login",
+    pathMatch: "full",
+  },
+  {
+    path: "login",
+    component: LoginComponent,
+  },
+  {
+    path: "registration",
+    component: RegisterComponent,
+  },
 ];
