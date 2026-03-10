92d3f5f943c2d884b167eb3edb2cfff518bc12ee
mahorobonheur <mahorobonheur@gmail.com>
2026-03-10 15:50:09 +0200

    feat: add rate limiting for login attempts

--- backend/src/main/java/com/amalitech/quickpoll/config/RedisConfig.java
+++ backend/src/main/java/com/amalitech/quickpoll/config/RedisConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -60,5 +61,17 @@ public class RedisConfig {
                 .withCacheConfiguration("polls",
                         defaultConfig.entryTtl(Duration.ofMinutes(2)))
                 .build();
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new StringRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
     }
 }
