2f80195375f19ef70cb1b87b37642e2212be053c
Gargoil <emmanueledusei.k@gmail.com>
2026-03-10 13:53:58 +0000

    Remove public GET access to polls endpoint

--- backend/src/main/java/com/amalitech/quickpoll/config/SecurityConfig.java
+++ backend/src/main/java/com/amalitech/quickpoll/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import com.amalitech.quickpoll.service.CustomUserDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -50,7 +49,6 @@ public class SecurityConfig {
                             "/v3/api-docs",
                             "/webjars/**"
                     ).permitAll()
-                    .requestMatchers(HttpMethod.GET, "/api/polls/**").permitAll()
                     .anyRequest().authenticated()
             )
                 .authenticationProvider(authenticationProvider())
