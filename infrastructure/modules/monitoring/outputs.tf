1817ee7030ff72d1219bf2f4fb1209c640a5783d
Viateur-akimana <akimanaviateur94@gmail.com>
2026-03-11 11:55:07 +0200

    fix(terraform): fix undeclared service discovery resource in monitoring outputs

--- infrastructure/modules/monitoring/outputs.tf
+++ infrastructure/modules/monitoring/outputs.tf
@@ -1,5 +1,5 @@
 
 output "service_discovery_namespace_id" {
   description = "ID of the Cloud Map private DNS namespace"
-  value       = aws_service_discovery_private_dns_namespace.monitoring.id
+  value       = var.service_discovery_namespace_id
 }
