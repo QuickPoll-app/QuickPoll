0e5cb070f3ea5dbedc68ded7fa65b1f55f3c7319
Broderick Nana Bentil <bentilfrederick4@gmail.com>
2026-03-12 16:23:06 +0000

    fix: revert apiUrl to default for development and production environments

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://k7mnnbfm-8081.uks1.devtunnels.ms/api'
-  // apiUrl: "https://67vhfctv-8081.uks1.devtunnels.ms/api",
-  // apiUrl: '/api'
+  apiUrl: "/api",
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://k7mnnbfm-8081.uks1.devtunnels.ms/",
-  // apiUrl: 'https://67vhfctv-8081.uks1.devtunnels.ms/api'
-  // apiUrl: '/api'
+  apiUrl: "/api",
 };
